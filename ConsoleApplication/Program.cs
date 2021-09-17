fced3a31cf5785ab22ee1833ecdbdfac2dbc3327
mtyra321 <mtyra321@gmail.com>
2021-09-17 11:45:00 -0600

    Add project files.

---
+++ ConsoleApplication/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            string location;
+            Console.WriteLine("Please enter your name");
+            name = Console.ReadLine();
+            Console.WriteLine("Please enter your location");
+            location = Console.ReadLine();
+            Console.WriteLine($"Your name is {name}");
+            Console.WriteLine($"Your location is {location}");
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
